63aa1116456d729796a99e352051767d7f4df954
matheus-vsm <scompinha@hotmail.com>
2025-11-30 00:50:00 -0300

    aula 4.13

---
+++ src/main/kotlin/fundamentos/4variaveis.kt
@@ -0,0 +1,14 @@
+package org.example.fundamentos
+
+class variaveis {
+    lateinit var teste: String
+    fun iniciar() {
+        teste = "teste"
+        println(teste)
+    }
+}
+
+fun main() {
+    var Var = "Mutavel, ou seja, pode mudar"
+    val Val = "Imutavel, ou seja, nao pode mudar"
+}
